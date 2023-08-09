2b21e96b7bd145b98355cc9543945006ff0a291e
Aaron Djangmah <djangsiness01@gmail.com>
2023-08-09 10:51:06 +0000

    updates

---
+++ lib/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GalleriaHomepage extends StatefulWidget {
+  const GalleriaHomepage({super.key});
+
+  @override
+  State<GalleriaHomepage> createState() => _GalleriaHomepageState();
+}
+
+class _GalleriaHomepageState extends State<GalleriaHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
